ce4ba9f348585c79a77eee67e7cbfadbc6589ca6
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2020-07-16 21:07:53 -0300

    Bubble Sort
    Criando um algoritmo de bubble sort

--- Aula03-Algoritmos/bubble_sort.c
+++ Aula03-Algoritmos/bubble_sort.c
@@ -8,23 +8,26 @@ int main(void)
     int array[12] = {4, 2, 5, 9, 23, 98, 122, 3, 6, 8, 7, 23};
     bubble(array);
     for (int i = 0; i < 12; i++) {
-        printf("O %i termo é %i\n", i, array[i]);
+        printf("O %iº termo é %i\n", i+1, array[i]);
     }
+    string s = get_string(" ");
+    printf("%s", s);
 }
 void bubble(int a[])
 {
     int l = len(a);
-    int count = 0;
-    while (count != l-1) {
-        for (int i = 0; i < l; i++) {
-            if (a[i-1] > a[i]) {
+    int counter = 1;
+    while (counter != 0)
+    {
+        counter = 0;
+        for (int i = 0; i < l-1; i++)
+        {
+            if (a[i] > a[i+1])
+            {
                 int temp = a[i];
-                a[i] = a[i-1];
-                a[i-1] = temp;
-                count = 0;
-            }
-            else {
-                count++;
+                a[i] = a[i+1];
+                a[i+1] = temp;
+                counter++;
             }
         }
     }

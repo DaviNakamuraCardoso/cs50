b889d9df22f7208e6d564179faaafffb8bf176a6
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2020-07-27 23:17:02 -0300

    Criando estruturas básicas
    Criando as funções básicas que compõem dictionary.c, a saber, load, hash, check, size e unload.

--- Aula05-Estruturas-de-Dados/speller/dictionary.c
+++ Aula05-Estruturas-de-Dados/speller/dictionary.c
@@ -1,7 +1,9 @@
 // Implements a dictionary's functionality
 
 #include <stdbool.h>
-
+#include <stdlib.h>
+#include <stdio.h>
+#include <strings.h>
 #include "dictionary.h"
 
 // Represents a node in a hash table
@@ -13,42 +15,126 @@ typedef struct node
 node;
 
 // Number of buckets in hash table
-const unsigned int N = 1;
+const unsigned int N = 1000;
+void destroy(node *n);
 
 // Hash table
 node *table[N];
-
 // Returns true if word is in dictionary else false
 bool check(const char *word)
 {
-    // TODO
+    int h = hash(word);
+    node *tmp = malloc(sizeof(node));
+    for (tmp = table[h]; tmp->next != NULL; tmp = tmp->next)
+    {
+        if ((strcasecmp(word, tmp->word)) == 0)
+        {
+            return true;
+            break;
+        }
+        else
+        {
+            continue;
+        }
+    }
     return false;
 }
 
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
-    // TODO
-    return 0;
+    unsigned int h;
+    int num = 0;
+    for (int i = 0; word[i] != '\0'; i++)
+    {
+        num += word[i];
+    }
+    h = N % num;
+    return h;
 }
 
 // Loads dictionary into memory, returning true if successful else false
 bool load(const char *dictionary)
 {
-    // TODO
-    return false;
+    FILE *d = fopen(dictionary, "r");
+    bool statement = false;
+    int c;
+    char *w = malloc((LENGTH+1)*sizeof(char));
+    int counter = 0;
+    int i = 0;
+    while ((c = fgetc(d)) != EOF)
+    {
+        if (c != '\n')
+        {
+            w[i] = c;
+            i++;
+        }
+        else
+        {
+            int h = hash(w);
+            if (table[h] == NULL)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    table[h] = malloc(sizeof(node));
+                    table[h]->word[j] = w[j];
+                    printf("%c", w[j]);
+                }
+            }
+            else
+            {
+                node* tmp = malloc(sizeof(node));
+                tmp->next = table[h]->next;
+                for (int j = 0; j < i; j++)
+                {
+                    tmp->word[j] = w[j];
+                    printf("%c", w[j]);
+                }
+                table[h]->next = tmp;
+            }
+            printf("\n");
+            i = 0;
+        }
+        counter++;
+    }
+    if (counter > 0)
+    {
+        statement = true;
+    }
+    return statement;
 }
 
 // Returns number of words in dictionary if loaded else 0 if not yet loaded
 unsigned int size(void)
 {
-    // TODO
-    return 0;
+    int siz = 0;
+    for (int i = 0; i < N; i++)
+    {
+        for (node *tmp = table[i]; tmp->next != NULL; tmp = tmp->next)
+        {
+            siz++;
+        }
+    }
+    return siz;
 }
-
 // Unloads dictionary from memory, returning true if successful else false
 bool unload(void)
 {
-    // TODO
+    for (int i = 0; i < N; i++)
+    {
+        destroy(table[i]);
+    }
     return false;
+}
+void destroy(node *n)
+{
+    if (n == NULL)
+    {
+        return;
+    }
+    else
+    {
+        destroy(n->next);
+    }
+    free(n);
 }

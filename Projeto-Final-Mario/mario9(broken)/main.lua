ab2353b7f982a86ecc403a25ed0eaa848a5e841d
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2020-08-19 22:22:23 -0300

    Mario 9
    Tentativa de inserção de detecção de colisões abaixo aos lados do personagem.

---
+++ Projeto-Final-Mario/mario9(broken)/main.lua
@@ -0,0 +1,103 @@
+--===================== My version of Mario made in Lua ======================--
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+-- Push library from https://github.com/Ulydev/push
+push = require "push"
+
+-- Class library from https://github.com/tenry92/class.luaw
+require("class")
+
+require 'utils'
+require 'Player'
+require 'Map'
+--============================================================================--
+
+
+
+function love.load()
+
+    math.randomseed(os.time())
+    -- Set the filter to no blurs
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    love.window.setTitle('Mario')
+
+    -- Loads the map
+    map = Map()
+
+    -- Set up the screen
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+            resizable = false,
+            fullscreen = false,
+            vsync = true
+    })
+
+    love.keyboard.keysPressed = {}
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+
+    end
+    love.keyboard.keysPressed[key] = true
+end
+
+function love.keyboard.wasPressed(key)
+    return love.keyboard.keysPressed[key]
+
+end
+
+function love.update(dt)
+    -- Udpates the map width
+    map:update(dt)
+
+    love.keyboard.keysPressed = {}
+end
+
+
+function love.draw()
+    push:apply('start')
+
+    love.graphics.clear(108 / 255, 140 / 255, 1, 1)
+    love.graphics.translate(math.floor(-map.camX), math.floor(-map.camY))
+
+    -- Renders the map
+    map:render()
+
+    push:apply('end')
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--============================================================================--

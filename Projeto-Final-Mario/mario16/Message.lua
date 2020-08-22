804ea2dc940ad0a44920fb7618ede4d1dd18f802
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2020-08-22 15:58:21 -0300

    Mario 15
    Movendo as operações de fim de level para a classe Player, o que permite maior precisão em sua construção, a saber, uma atualização automática das posições do mapa e reset do score e do número de caixas a cada level sem necessidade de fazê-lo ativamente.

---
+++ Projeto-Final-Mario/mario16/Message.lua
@@ -0,0 +1,35 @@
+Message = Class()
+
+
+function Message:construct(x, y, font, size, color)
+    self.x = x
+    self.y = y
+    self.font = font
+    self.size = size
+    self.color = color
+    self.counter = 0
+    self.parameter = VIRTUAL_WIDTH
+    self.sound = love.audio.newSource('sounds/score.wav', 'stream')
+end
+
+
+function Message:show(text)
+    currentFont = love.graphics.getFont()
+    messageFont = love.graphics.newFont(self.font, self.size)
+    love.graphics.setFont(messageFont)
+    currentColor = love.graphics.getColor()
+    love.graphics.setColor(self.color[1], self.color[2], self.color[3], self.color[4])
+    love.graphics.printf(text, map.camX + self.x, map.camY + self.y, self.parameter, 'center')
+    love.graphics.setFont(currentFont)
+
+end
+
+
+
+function Message:prettyShow(times, sound, message, velocity)
+    if self.counter < times then
+        self.counter = self.counter + velocity
+        sound:play()
+    end
+    self:show(message .. ' ' ..tostring(self.counter))
+end

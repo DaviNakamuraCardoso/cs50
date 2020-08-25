44cca05b6f53086b04c78973a5ea1c6c8c155ed4
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2020-08-25 17:27:38 -0300

    Mario 24
    Adicionando uma nova mecânica de jogo que permite o jogador mate inimigos ao pular em caixas sobre as quais eles estão.

---
+++ Projeto-Final-Mario/mario24/Message.lua
@@ -0,0 +1,57 @@
+Message = Class()
+
+
+function Message:construct(x, y, font, size, color)
+    self.x = x
+    self.y = y
+    self.font = font
+    self.size = size
+    self.color = {}
+    for i=1, 4 do
+        self.color[i] = color[i]
+    end 
+    self.counter = 0
+    self.dt = 0
+    self.parameter = VIRTUAL_WIDTH
+    self.sound = love.audio.newSource('sounds/score.wav', 'stream')
+end
+
+
+function Message:show(text)
+    currentFont = love.graphics.getFont()
+    messageFont = love.graphics.newFont(self.font, self.size)
+    love.graphics.setFont(messageFont)
+    currentColor = love.graphics.getColor()
+    love.graphics.setColor(self.color[1], self.color[2], self.color[3], self.color[4])
+    love.graphics.printf(text, map.camX + self.x, map.camY + self.y, self.parameter, 'center')
+    love.graphics.setFont(currentFont)
+    love.graphics.setColor(255, 255, 255, 255)
+
+end
+
+
+
+function Message:prettyShow(times, sound, message, velocity)
+    if self.counter < times then
+        self.counter = self.counter + velocity
+        sound:play()
+    end
+    self:show(message .. ' ' ..tostring(self.counter))
+end
+
+
+function Message:twinkle(message, interval, sound)
+    if self.dt > interval then
+        sound:play()
+        self.dt = 0
+        self.counter = self.counter + 1
+        self.color[4] = (self.counter) % 2
+    end
+    self:show(message)
+end
+
+
+
+function Message:update(dt)
+    self.dt = self.dt + dt
+end

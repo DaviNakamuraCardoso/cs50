88ccd8da53e7153c9b4158b8897839edbc5c6f09
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2020-08-28 15:36:57 -0300

    Mario 32
    1. Correção de bugs que faziam os botões se comportarem de modo incorreto;
    2. Correção de um bug que fazia o jogo entrar em game over sem que o player tivesse perdido todas as vidas.

---
+++ Projeto-Final-Mario/mario32/Button.lua
@@ -0,0 +1,42 @@
+--============================ Button Class =================================--
+require('superclass')
+Button = Class(Block)
+
+--============================================================================--
+
+
+function Button:construct(map, x, y, width, height, color)
+    Button.super.construct(self, map, x, y, width, height, color)
+    self.map = map
+    self.noHoverColor = color
+    self.hoverColor = {
+        self.color[1] / 2, self.color[2] / 2, self.color[3] / 2, self.color[4]
+    }
+    self.clicked = false
+
+
+
+end
+
+function Button:hover()
+    mouseX = love.mouse.getX() * VIRTUAL_WIDTH / love.graphics.getWidth()
+    mouseY = love.mouse.getY() * VIRTUAL_HEIGHT / love.graphics.getHeight()
+    if self.x <= mouseX and mouseX <= self.x + self.width and self.y <= mouseY and mouseY <= self.y + self.height then
+        self.color = self.hoverColor
+        return true
+    else
+        self.color = self.noHoverColor
+    end
+end
+
+
+
+function Button:update()
+    self:getCamCoordinates(self.map)
+    if self:hover() and mouse['clicked'] then
+        self.clicked = true
+    else
+        self.clicked = false
+    end
+
+end
